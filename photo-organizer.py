630a1d377caa3524b6d1d55fd9bb14d38ad3105c
chrissutanto <chris.sutanto28@gmail.com>
2017-12-21 19:25:59 -0800

    test

---
+++ photo-organizer.py
@@ -0,0 +1,26 @@
+import shutil, os, hashlib
+
+dictionary = open("dictionary.txt", "w")
+
+def parseDirectory(directory):
+    for root, dirs, files in os.walk(directory):
+        print("root " + str(root))
+        for filename in files:
+            print("file " + str(filename))
+            writeToDictionary(os.path.join(root, filename))
+
+
+def writeToDictionary(location):
+    hash = hashlib.sha1()
+    bufferSize = 65536
+    with open(location, 'rb') as file:
+        buffer = file.read(bufferSize)
+        while len(buffer) > 0:
+            hash.update(buffer)
+            buffer = file.read(bufferSize)
+    print(os.path.relpath(location, startDir) + " : " + str(hash.hexdigest()) + os.linesep)
+    dictionary.write(os.path.relpath(location, startDir) + " : " + str(hash.hexdigest()) + os.linesep)
+
+
+startDir = 'C:/Users/chris/Documents/Python Projects/Test/testfldr'
+parseDirectory(startDir)
